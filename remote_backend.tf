463de333d09bac5117ba6a0b265d5b73528277e4
paolajuarezgomez <paola.juarez.gomez@gmail.com>
2022-05-17 12:05:39 +0200

    changes

--- remote_backend.tf
+++ remote_backend.tf
@@ -3,10 +3,10 @@ terraform {
     bucket   = "terraform-backend"
     key      = "terraform.tfstate"
     # Add your region
-    region   = "uk-london-1"
+    region   = "eu-frankfurt-1"
     # https://<namespace>.compat.objectstorage.<region>.oraclecloud.com
     # Modiy the endpoint value with your data.
-    endpoint = "https://frxfz3gch4zb.compat.objectstorage.uk-london-1.oraclecloud.com"
+    endpoint = "https://frxfz3gch4zb.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
     shared_credentials_file     = "./cred_store"
     skip_region_validation      = true
     skip_credentials_validation = true
