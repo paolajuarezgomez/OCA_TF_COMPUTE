8a38eb1b1a859de81abf998e15a1c93b48470af5
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-05-17 09:33:10 +0200

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,7 +1,6 @@
 
 
 resource "oci_objectstorage_bucket" "test_bucket" {
-  count = var.create_bucket == "True" ? 1 : 0
   #Required
   compartment_id = var.default_compartment_id
   name           = "${var.names_prefix}bucket"
