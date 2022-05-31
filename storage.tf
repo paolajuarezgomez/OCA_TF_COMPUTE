334526db7f3b0baaaec89e08bfbe6648e6d64085
paolajuarezgomez <paola.juarez.gomez@gmail.com>
2022-05-31 13:29:37 +0200

    changes

--- storage.tf
+++
@@ -1,9 +0,0 @@
-
-
-resource "oci_objectstorage_bucket" "test_bucket" {
-  #Required
-  compartment_id = var.default_compartment_id
-  name           = "${var.names_prefix}bucket"
-  namespace      = data.oci_objectstorage_namespace.user_namespace.namespace
-
-}
