ab5a240791d24c296110298351c526c57d6d3f58
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-06-03 09:51:11 +0200

    Update and rename main.tf to compute.tf

--- compute.tf
+++ compute.tf
@@ -15,7 +15,7 @@ resource "oci_core_instance" "test_instance" {
     subnet_id                 = var.subnet_id
   }
   source_details {
-    source_id   = data.oci_core_app_catalog_listing_resource_version.test_catalog_listing.listing_resource_id
+    source_id   = data.oci_core_images.this.images[0].id
     source_type = "image"
   }
   metadata = {
