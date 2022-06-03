308952bfeb87ca7a4e28b16ae90327a0314fc07f
Paola Juárez <41003502+paolajuarezgomez@users.noreply.github.com>
2022-06-03 09:52:23 +0200

    Update datasource.tf

--- datasource.tf
+++ datasource.tf
@@ -3,31 +3,10 @@ data "oci_identity_user" "coa_demo_executer" {
   user_id = var.user_id
 }
 
-data "oci_core_app_catalog_listing_resource_version" "test_catalog_listing" {
-  listing_id       = data.oci_marketplace_listing_package.test_listing_package.app_catalog_listing_id
-  resource_version = data.oci_marketplace_listing_package.test_listing_package.app_catalog_listing_resource_version
-}
+data "oci_core_images" "this" {
+  #Required
+  compartment_id = var.default_compartment_id
 
-data "oci_marketplace_listing_package" "test_listing_package" {
-  listing_id      = data.oci_marketplace_listing.test_listing.id
-  package_version = data.oci_marketplace_listing.test_listing.default_package_version
-  compartment_id  = var.compartment_id
-}
-
-data "oci_marketplace_listing_package_agreements" "test_listing_package_agreements" {
-  listing_id      = data.oci_marketplace_listing.test_listing.id
-  package_version = data.oci_marketplace_listing.test_listing.default_package_version
-  compartment_id  = var.compartment_id
-}
-
-
-data "oci_marketplace_listing" "test_listing" {
-  listing_id     = data.oci_marketplace_listings.test_listings.listings[0].id
-  compartment_id = var.compartment_id
-}
-
-
-data "oci_marketplace_listings" "test_listings" {
-  name           = [var.source_name]
-  compartment_id = var.compartment_id
+  #Optional
+  display_name = var.image_name
 }
