686a5ef14d3e1ffbdff7e91d55924d6048c9f063
paolajuarezgomez <paola.juarez.gomez@gmail.com>
2022-06-03 10:04:39 +0200

    changes

--- outputs.tf
+++ outputs.tf
@@ -7,33 +7,7 @@ output "availability_Domain" {
   value       = oci_core_instance.test_instance.availability_domain
 }
 
-output "listing_id" {
-  description = ""
-  value       = data.oci_marketplace_listing.test_listing.id
-}
 
-output "package_version" {
-  description = "default package version"
-  value       = data.oci_marketplace_listing.test_listing.default_package_version
-}
 
-output "source_id" {
-  description = "ocid source"
-  value       = data.oci_core_app_catalog_listing_resource_version.test_catalog_listing.listing_resource_id
-}
-
-output "app_catalog" {
-  description = ""
-  value       = data.oci_marketplace_listing_package.test_listing_package.app_catalog_listing_id
-}
-
-output "oci_marketplace_listings" {
-  description = ""
-  sensitive   = false
-  value = [
-    format("Listing name: %s", data.oci_marketplace_listings.test_listings.listings[0].name),
-    format("Package Type: %s", data.oci_marketplace_listings.test_listings.listings[0].package_type)
-  ]
-}
 
 
