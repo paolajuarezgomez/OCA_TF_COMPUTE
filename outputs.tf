f2d7ae893fedef76d6e5c3825511bcf7abd3c1a3
paolajuarezgomez <paola.juarez.gomez@gmail.com>
2022-06-03 11:59:32 +0200

    changeschang

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,8 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 
-output "availability_Domain" {
-  description = "availability_domain"
-  value       = oci_core_instance.deploy_instance.availability_domain
+output "compute" {
+  value       = oci_core_instance.deploy_instance.agent_config
 }
 
 
